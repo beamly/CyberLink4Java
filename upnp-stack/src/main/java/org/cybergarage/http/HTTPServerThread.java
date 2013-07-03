7511a2357721c5294b1b3e1915c18a0da9ace144
Chris Warren <chris@ixalon.net>
2013-07-03 16:33:39 +0100

    Fix NPE from NotifyRequest.getPropertyList() - swallow (and output) any exceptions in HTTPServerThread when processing incoming requests.

--- upnp-stack/src/main/java/org/cybergarage/http/HTTPServerThread.java
+++ upnp-stack/src/main/java/org/cybergarage/http/HTTPServerThread.java
@@ -1,34 +1,33 @@
 /******************************************************************
-*
-*	CyberHTTP for Java
-*
-*	Copyright (C) Satoshi Konno 2002-2003
-*
-*	File: HTTPServerThread.java
-*
-*	Revision;
-*
-*	10/10/03
-*		- first revision.
-*	
-******************************************************************/
+ * 
+ * CyberHTTP for Java
+ * 
+ * Copyright (C) Satoshi Konno 2002-2003
+ * 
+ * File: HTTPServerThread.java
+ * 
+ * Revision;
+ * 
+ * 10/10/03 - first revision.
+ * 
+ ******************************************************************/
 
 package org.cybergarage.http;
 
 import java.net.Socket;
 
-public class HTTPServerThread extends Thread
-{
+import org.cybergarage.util.Debug;
+
+public class HTTPServerThread extends Thread {
 	private HTTPServer httpServer;
 	private Socket sock;
-	
+
 	////////////////////////////////////////////////
 	//	Constructor
 	////////////////////////////////////////////////
-	
-	public HTTPServerThread(HTTPServer httpServer, Socket sock)
-	{
-        super("Cyber.HTTPServerThread");
+
+	public HTTPServerThread(HTTPServer httpServer, Socket sock) {
+		super("Cyber.HTTPServerThread");
 		this.httpServer = httpServer;
 		this.sock = sock;
 	}
@@ -37,15 +36,19 @@ public class HTTPServerThread extends Thread
 	//	run	
 	////////////////////////////////////////////////
 
-	public void run()
-	{
+	public void run() {
 		HTTPSocket httpSock = new HTTPSocket(sock);
 		if (httpSock.open() == false)
 			return;
 		HTTPRequest httpReq = new HTTPRequest();
 		httpReq.setSocket(httpSock);
 		while (httpReq.read() == true) {
-			httpServer.performRequestListener(httpReq);
+			try {
+				httpServer.performRequestListener(httpReq);
+			} catch (Exception ex) {
+				Debug.warning("");
+				Debug.warning(ex);
+			}
 			if (httpReq.isKeepAlive() == false)
 				break;
 		}
